4871153a0c37f585c5e64c9007a78f5fed265cdd
Bigyayos <pramosbana@gmail.com>
2024-12-10 19:38:26 +0100

    Add files via upload

--- hooligans-golf-client/src/App.js
+++ hooligans-golf-client/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Jugadores from './pages/Jugadores';
-import Torneos from './pages/Torneos';
-import Tarjetas from './pages/Tarjetas';
-import Resultados from './pages/Resultados';
-import Navbar from './components/Navbar';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/jugadores" element={<Jugadores />} />
-        <Route path="/torneos" element={<Torneos />} />
-        <Route path="/tarjetas" element={<Tarjetas />} />
-        <Route path="/resultados" element={<Resultados />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
