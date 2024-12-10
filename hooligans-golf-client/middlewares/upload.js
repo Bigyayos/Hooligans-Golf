9f05579384d13135bca5356ab01baecc33f66954
Bigyayos <pramosbana@gmail.com>
2024-12-10 19:35:51 +0100

    Add files via upload

---
+++ hooligans-golf-client/middlewares/upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error('Solo se permiten archivos de imagen (jpeg, jpg, png, gif)'));
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 } // Limitar el tamaño del archivo a 5MB
+});
+
+module.exports = upload;
